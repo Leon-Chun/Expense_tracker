1d7b5a006bed299870b50f8ee7753e8159c6069b
Leon-Chun <s1235753@gmail.com>
2022-11-08 15:49:32 +0800

    feat: add exprss-handlebars

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 3000
 
 //使用套件後產生的
 const app = express()
+
+//handlebars setting
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')  //啟用引擎
 
 // user body get 
 app.use(express.urlencoded({ extended: true }))
 
 //router setting
 app.get('/', (req, res) => {
-  res.send('This is my test page for initialization(初始化) ')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
