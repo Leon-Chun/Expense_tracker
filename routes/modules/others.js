d9b3cfb25ff51955dd9b6899629acf1e5c5b81e0
Leon-Chun <s1235753@gmail.com>
2022-11-09 17:40:46 +0800

    feat: modify edit page

--- routes/modules/others.js
+++ routes/modules/others.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router() // 啟動路由器功能
 const Recort = require('../../models/record')
+const moment = require('moment')
 
-
+//首頁
 router.get('/new', (req, res) => {
   res.render('new')
   // Todo.find()
@@ -12,12 +13,40 @@ router.get('/new', (req, res) => {
   //   .catch(error => console.error(error))
 })
 
+//新增
 router.post('/new', (req, res) => {
   // const userId = req.user._id
   return Recort.create({ ...req.body }) //,userId
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+
+//修改
+router.get('/:id/edit',(req,res) => {
+  const _id = req.params.id
+  return Recort.findById({ _id }) //,userId
+    .lean()
+    // .populate('category')
+    .then(record => {
+      record.date = moment(record.date).format('YYYY-MM-DD')
+      // categories.forEach(category => {
+      //   if (category._id.toString() === record.category._id.toString()) {
+      //     category.selected = 'selected'
+      //   }
+      // })
+      res.render('edit', { record })// , categories
+    })
+    .catch(error => console.log(error))
+})
+
+// router.put('/:id', (req, res) => {
+//   // const userId = req.user._id
+//   const _id = req.params.id
+//   return Recort.findById({ _id }) //,userId
+//     .then(record => record.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 //刪除
 router.delete('/:id', (req, res) => {
