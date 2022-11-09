117cf6601d5df03b37b76865b8a65381889a3e70
Leon-Chun <s1235753@gmail.com>
2022-11-09 15:52:31 +0800

    feat:add moment and modify index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const router = express.Router() // 啟動路由器功能
 const record = require('../../models/record')
+const moment = require('moment')
 
 router.get('/', (req, res) => {
-  res.render('index')
-  // Todo.find()
-  //   .lean()
-  //   .sort({ name: 'asc' }) //desc 反序
-  //   .then(todos => res.render('index', { todos }))
-  //   .catch(error => console.error(error))
+  record.find()
+    .lean()
+    // .sort({ name: 'asc' }) //desc 反序
+    .then(data => {
+      data.forEach(record => {
+        record.date = moment(record.date).format('YYYY-MM-DD')
+        // totalAmount += record.amount
+      })
+      res.render('index', { data }) //, categories , totalAmount
+    })
+    .catch(error => console.error(error))
 })
 
 module.exports = router
--- routes/modules/others.js
+++ routes/modules/others.js
@@ -4,11 +4,16 @@ const Recort = require('../../models/record')
 
 router.get('/new', (req, res) => {
   res.render('new')
- 
+  // Todo.find()
+  //   .lean()
+  //   .sort({ name: 'asc' }) //desc 反序
+  //   .then(todos => res.render('index', { todos }))
+  //   .catch(error => console.error(error))
 })
 
 router.post('/new', (req, res) => {
-  return Recort.create({ ...req.body }) 
+  // const userId = req.user._id
+  return Recort.create({ ...req.body }) //,userId
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
