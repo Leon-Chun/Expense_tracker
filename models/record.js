a0bafcaae679a627bceb674950f6eab647b5df59
Leon-Chun <s1235753@gmail.com>
2022-11-09 12:26:27 +0800

    feat: add recordSchema

---
+++ models/record.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  amount: { type: Number, required: true },
+  categoryId: { type: Number, ref:'Category' , index: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+  createdAd: { type: Date, default: Date.now } 
+})
+
+module.exports = mongoose.model('Record', recordSchema)
