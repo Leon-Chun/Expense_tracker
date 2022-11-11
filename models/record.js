e22aef9e02f9ea0454a4b7656db30e76cd6d64fb
Leon-Chun <s1235753@gmail.com>
2022-11-11 16:00:15 +0800

    feat: add seeder

--- models/record.js
+++ models/record.js
@@ -5,15 +5,9 @@ const recordSchema = new Schema({
   name: { type: String, required: true },
   date: { type: Date, required: true },
   amount: { type: Number, required: true },
-  categoryId: { type: Number, ref:'Category' , index: true },
-  userId: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+  categoryId: { type: Number, ref: 'Category', index: true, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', index: true, required: true },
   createdAd: { type: Date, default: Date.now },
-  userId: { 
-    type: Schema.Types.ObjectId,    //`type` 和 `ref` 這兩個設定是一起
-    ref: 'User',  //定義參考對象是 User model
-    index: true,   //增加讀取效能
-    required: true
-  }
 })
 
 module.exports = mongoose.model('Record', recordSchema)
---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const Record = require('../record')
+const User = require('../user')
+const db = require('../../config/mongoose')
+
+const SEED_USERS = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}]
+
+const itemsSeeder = [{
+  name: '藍筆',
+  date: '2022-08-01',
+  categoryId: 5,
+  amount: 15,
+}, {
+  name: '我的生日',
+  date: '2022-09-21',
+  categoryId: 4,
+  amount: 100000,
+}, {
+  name: '慢喂門票',
+  date: '2022-08-08',
+  categoryId: 3,
+  amount: 300,
+}, {
+  name: '電競椅',
+  date: '2022-11-15',
+  categoryId: 1,
+  amount: 20000,
+}, {
+  name: '股票講座',
+  date: '2018-09-05',
+  categoryId: 2,
+  amount: 500,
+}]
+
+
+db.once('open', () => {
+  Promise.all(SEED_USERS.map((SEED_USER) =>
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER.password, salt))
+
+      .then(hash => User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      }))
+
+      .then(user => {
+        const userId = user._id
+        return Promise.all(itemsSeeder.map(record => {
+          return Record.create({ ...record, userId })
+        }))
+      })
+  ))
+
+    .then(() => {
+      console.log('recordSeeder done.')
+      process.exit()
+    })
+})
